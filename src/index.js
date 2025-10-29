0a56926a5dfcb388ce6db197d234d822125b7a5c
Morgan Leek <morganleek@gmail.com>
2025-10-29 11:27:44 +0800

    nav

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import './style.scss';
 // import { tns } from "tiny-slider"
 
 document.addEventListener('DOMContentLoaded', () => {
+	// Prevent nav animations running when page first loaded
+	setTimeout( () => {
+		document.body.classList.add( "is-loaded" );
+	}, 500 );
+
 	// Lazy load fade in
 	document.querySelectorAll( 'img[loading="lazy"]' ).forEach( ( img ) => {
 		if( img.complete === true ) {
@@ -18,5 +23,13 @@ document.addEventListener('DOMContentLoaded', () => {
 	// Copyright Year
 	document.querySelectorAll(".copyright").forEach( ( p ) => { 
 		p.innerHTML = p.innerHTML.replace( '{YEAR}', new Date().getUTCFullYear() );
+	} );
+
+	// Nav Trigger 
+	document.querySelectorAll( ".navigation-trigger a" ).forEach( link => { 
+		link.addEventListener( "click", e => {
+			e.preventDefault();
+			document.body.classList.toggle( "nav-open" );
+		} );
 	} );
 });
