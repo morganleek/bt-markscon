a99c54cd846767698142f9ee6152baf7c6d84b4e
Morgan Leek <morganleek@gmail.com>
2025-11-11 22:46:57 +0800

    home

--- functions.php
+++ functions.php
@@ -110,4 +110,47 @@
 			'name' => 'special-appearance',
 			'label' => __( 'Special', 'bones_name' ),
 		] );
+
+		register_block_style( 'core/group', [
+			'name' => 'clip-bg-top-right',
+			'label' => 'Clip ◥'
+		] );
+
+		register_block_style( 'core/group', [
+			'name' => 'clip-bg-top-left',
+			'label' => 'Clip ◤'
+		] );
+
+		register_block_style( 'core/cover', [
+			'name' => 'clip-bg-right',
+			'label' => 'Clip ◥'
+		] );
 	}
+
+	// Add am icon
+	function add_an_icon( $icons ) {
+		// Icon must include width + height in both the SVG and array values
+		$icons[] = [
+			'label' => 'Arrow Left',
+			'icon' 	=> "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 23 14' width='23' height='14'><path d='M23.56-15.34l-6.99,6.99-.87-.87,5.52-5.51H1.11v-1.22h20.11l-5.52-5.52.87-.87,6.99,6.99ZM7.27,13.99l.87-.87L2.62,7.61h20.11v-1.22H2.62L8.13.87,7.27,0,.27,7l6.99,6.99ZM62.52-13.36l-5.52,5.52v-16.11h-1.22V-7.85l-5.51-5.52-.87.87,6.99,6.99,6.99-6.99-.87-.87Z'/></svg>",
+			'width' => '23',
+			'height' => '14',
+			'value' => 'markson-arrow-left'
+		];
+		$icons[] = [
+			'label' => 'Arrow Right',
+			'icon' 	=> "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 23 14' width='23' height='14'><path d='M22.73,7l-6.99,6.99-.87-.87,5.52-5.51H.27v-1.22h20.11L14.87.87,15.73,0l6.99,6.99ZM6.43,36.34l.87-.87-5.52-5.52h20.11v-1.22H1.78l5.52-5.51-.87-.87L-.56,29.34l6.99,6.99ZM61.68,8.98l-5.52,5.52V-1.61h-1.22V14.49l-5.51-5.52-.87.87,6.99,6.99,6.99-6.99-.87-.87Z'/></svg>",
+			'width' => '23',
+			'height' => '14',
+			'value' => 'markson-arrow-right'
+		];
+		$icons[] = [
+			'label' => 'Arrow Down',
+			'icon' 	=> "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 14 19' width='14' height='19'><path d='M-25.83,8.89l-6.99,6.99-.87-.87,5.52-5.51h-20.11v-1.22h20.11l-5.52-5.52.87-.87,6.99,6.99ZM-42.12,38.22l.87-.87-5.52-5.52h20.11v-1.22h-20.11l5.52-5.51-.87-.87-6.99,6.99,6.99,6.99ZM13.13,10.87l-5.52,5.52V.27h-1.22v16.11L.87,10.87,0,11.73l6.99,6.99,6.99-6.99-.87-.87Z'/></svg>",
+			'width' => '14',
+			'height' => '19',
+			'value' => 'markson-arrow-down'
+		];
+		return $icons;
+	}
+	add_filter( 'enable_button_icons_icons_update', 'add_an_icon' ,  20, 1 );
