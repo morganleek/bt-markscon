26640cff143128bd1851b50902ea61204fb4b2cf
Morgan Leek <morganleek@gmail.com>
2026-06-11 11:18:50 +0800

    slide in show

--- src/index.js
+++ src/index.js
@@ -162,7 +162,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		resizeTimer = setTimeout( initRedLine, 250 );
 	} );
 
-	document.querySelectorAll( "main > .entry-content > .wp-block-group > *, main > .entry-content > .wp-block-columns > .wp-block-column > *, main > .entry-content > .wp-block-media-text" ).forEach( block => {
+	document.querySelectorAll( "main > .entry-content > .wp-block-group > *, main > .entry-content > .wp-block-columns > .wp-block-column > *, main > .entry-content > .wp-block-media-text, .home-cover > .wp-block-cover > .wp-block-cover__inner-container > .wp-block-group > *" ).forEach( block => {
 		gsap.timeline({
 			scrollTrigger: {
 				scrub: 1,
